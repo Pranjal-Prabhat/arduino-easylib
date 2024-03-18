06c3130ef7c358dba28bf6c76c17a44253066ad9
Pranjal-Prabhat <162833701+Pranjal-Prabhat@users.noreply.github.com>
2024-03-18 21:00:25 +0530

    Update easylib.cpp
    Signed-off-by: Pranjal-Prabhat <162833701+Pranjal-Prabhat@users.noreply.github.com>

--- easylib/easylib.cpp
+++ easylib/easylib.cpp
@@ -1,6 +1,22 @@
 #include <Arduino.h>
 #include "easylib.h"
 
+int sonar(byte trig, byte echo, byte vcc = 0) {
+  if (vcc > 0) {
+    digitalWrite(vcc, HIGH); 
+  }
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  long duration = pulseIn(echo, HIGH);
+  int distance = duration /29/2; 
+  return distance;
+}
+
 void l298n(byte array[4],bool arr[4],byte ctrl_pin1 = 0, byte ctrl_pin2 = 0,byte value1 = 255,byte value2 = 255){
   digitalWrite(array[0],arr[0]);
   digitalWrite(array[1],arr[1]);
@@ -20,35 +36,6 @@ void sonarinit(byte trig_pin,byte echo_pin,byte vcc=-1){
   pinMode(echo_pin,INPUT);
 }
 
-void serialinit(int init){
-  Serial.begin(init);
-}
-
-void print(String str) {
-   Serial.print(String(str)); }
-
-void println(String str){
- Serial.println(String(str));}
-void on(byte pin){
-    digitalWrite(pin, 1);
-}
-
-int sonar(byte trig, byte echo, byte vcc = 0) {
-  if (vcc > 0) {
-    digitalWrite(vcc, HIGH); 
-  }
-
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-
-  long duration = pulseIn(echo, HIGH);
-  int distance = duration /29/2; 
-  return distance;
-}
-
 bool ir(byte pin , bool config=false){
   if (config = false){
   bool var =  digitalRead(pin);
@@ -65,6 +52,20 @@ void servo(byte degree, byte pin, bool error=false){
     if (degree < 181 && degree > 0){
         degree = degree / 180.0 * 255;
     }
+}
+
+void serialinit(int init){
+  Serial.begin(init);
+}
+
+void print(String str) {
+   Serial.print(String(str)); }
+
+void println(String str){
+ Serial.println(String(str));}
+
+void on(byte pin){
+    digitalWrite(pin, 1);
 }
 
 void off(byte pin){
