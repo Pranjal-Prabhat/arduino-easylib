4ed5f1025c036f6920eca603df35ff4928fdf963
Pranjal-Prabhat <162833701+Pranjal-Prabhat@users.noreply.github.com>
2024-03-22 17:39:36 +0530

    Update easylib.cpp
    Signed-off-by: Pranjal-Prabhat <162833701+Pranjal-Prabhat@users.noreply.github.com>

--- easylib/easylib.cpp
+++ easylib/easylib.cpp
@@ -1,22 +1,6 @@
 #include <Arduino.h>
 #include "easylib.h"
 
-int sonar(byte trig, byte echo, byte vcc = 0) {
-  if (vcc > 0) {
-    digitalWrite(vcc, HIGH); 
-  }
-
-  digitalWrite(trig, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-
-  long duration = pulseIn(echo, HIGH);
-  int distance = duration /29/2; 
-  return distance;
-}
-
 void l298n(byte array[4],bool arr[4],byte ctrl_pin1 = 0, byte ctrl_pin2 = 0,byte value1 = 255,byte value2 = 255){
   digitalWrite(array[0],arr[0]);
   digitalWrite(array[1],arr[1]);
@@ -34,6 +18,22 @@ void sonarinit(byte trig_pin,byte echo_pin,byte vcc=-1){
   }
   pinMode(trig_pin,OUTPUT);
   pinMode(echo_pin,INPUT);
+}
+
+int sonar(byte trig, byte echo, byte vcc = 0) {
+  if (vcc > 0) {
+    digitalWrite(vcc, HIGH); 
+  }
+
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+
+  long duration = pulseIn(echo, HIGH);
+  int distance = duration /29/2; 
+  return distance;
 }
 
 bool ir(byte pin , bool config=false){
@@ -54,6 +54,18 @@ void servo(byte degree, byte pin, bool error=false){
     }
 }
 
+void ledon(){
+  pinMode(13,OUTPUT);
+  digitalWrite(13,1);
+  pinMode(13,NULL);
+}
+
+void ledoff(){
+  pinMode(13,OUTPUT);
+  digitalWrite(13,1);
+  pinMode(13,NULL);
+}
+
 void serialinit(int init){
   Serial.begin(init);
 }
@@ -63,9 +75,44 @@ void print(String str) {
 
 void println(String str){
  Serial.println(String(str));}
-
 void on(byte pin){
     digitalWrite(pin, 1);
+}
+
+void pause(int number){
+  delay(number*1000);
+}
+
+void clock(){
+ return millis(); 
+}
+
+void mintosec(int min){
+  return min*60;
+}
+
+void getdistance(int speed , int time){
+  return speed/time;
+}
+
+void  mintoms(int min){
+  return min*1000*60;
+}
+
+void sectomin(int second){
+  return second/60;
+}
+
+void  mstomin(int ms){
+  return ms/1000/60;
+}
+
+void  mstosec(int ms){
+  return ms/1000;
+}
+
+void  sectoms (int second){
+  return second*1000;
 }
 
 void off(byte pin){
@@ -75,7 +122,6 @@ void off(byte pin){
 void set(byte pin, bool value){
     pinMode(pin, value);
 }
-
 
 void pwm(byte pin , byte value){
     analogWrite(pin, value);
@@ -93,3 +139,4 @@ bool digiread(byte pin){
 int anaread(byte pin){
   return analogRead(pin);
 }
+
